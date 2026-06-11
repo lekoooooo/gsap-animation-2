e0d32224d439d6031986f90d150396e30483955e
lekoooooo <levdidebashvili@gmail.com>
2026-06-11 16:34:13 +0400

    Update HouseScroll.jsx

--- src/component/HouseScroll.jsx
+++ src/component/HouseScroll.jsx
@@ -5,33 +5,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
-const HouseScroll = () => {
-  const containerRef = useRef(null);
-  const [svgLoaded, setSvgLoaded] = useState(false);
-  const animationRef = useRef(null);
 
-  useEffect(() => {
-    const loadSVG = async () => {
-      try {
-        const response = await fetch("house.svg");
-        const svgText = await response.text();
-
-        if (containerRef.current) {
-          containerRef.current.innerHTML = svgText;
-
-          const svgElement = containerRef.current.querySelector("svg");
-          if (svgElement) {
-            svgElement.setAttribute("preserveAspectRatio", "xMidYMid slice");
-            requestAnimationFrame(() => setSvgLoaded(true));
-          }
-        }
-      } catch (error) {
-        console.error("error:", error);
-      }
-    };
-
-    loadSVG();
-  }, []);
 
   useGSAP(
     () => {
